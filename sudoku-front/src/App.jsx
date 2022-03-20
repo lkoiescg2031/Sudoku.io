0f79d2f73dc5b04cbeff6e3479965a9e0f0c0c56
Taehong Kim <lkoiescg2031@naver.com>
2022-03-20 13:34:57 +0900

    refactor : api 모듈화

--- sudoku-front/src/App.jsx
+++ sudoku-front/src/App.jsx
@@ -1,55 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import "./App.scss";
+import {fetchSudoku} from "./api/sudoku";
 
 function random(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function App() {
-	// 만들 변수 및 변수 세팅할 때 useState사용
-	const [loading, setLoading] = useState(null);
-	const [error, setError] = useState(null);
-	const [arrs, setArrs] = useState(null);
+    // 만들 변수 및 변수 세팅할 때 useState사용
+    const [loading, setLoading] = useState(null);
+    const [error, setError] = useState(null);
+    const [arrs, setArrs] = useState(null);
 
-	useEffect(() => {
-		const fetchArr = async () => {
-			try {
-				const rand = random(18, 81);
+    useEffect(() => {
+        const fetchArr = async () => {
+            try {
+                const rand = random(18, 81);
 
-				setError(null);
-				setArrs(null);
-				setLoading(null);
-				const response = await axios.get(
-					`http://localhost:8080/api/v1/sudoku/random?showCount=${rand}`
-				);
-				setArrs(response.data);
-				
-			} catch (e) {
-				console.log(e);
-				setError(e);
-			}
-			setLoading(false);
-		};
-		fetchArr();
-	  }, []);
-	
-	if (loading) return <div>로딩중..</div>;
-	if (error) return <div>{error}</div>;
-	// 연습
-	if (!arrs) return null;
-	console.log(arrs)
-	
-	return (
-		<>
-			{/* {JSON.stringify(arrs)} */}
-			<div className="sdk">
-				{arrs.map.map(arr => (
-					{arr}
-				))}
-			</div>
-		</>
-	);
+                setError(null);
+                setArrs(null);
+                setLoading(true);
+
+                const response = await fetchSudoku({params: {showCount: {rand}}});
+                setBoard(response.data.map);
+            } catch (e) {
+                setError(e);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchArr();
+    }, []);
+
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>{error}</div>;
+    // 연습
+    if (!arrs) return null;
+
+    return (<>
+        <div className="sdk">
+            {arrs.map(arr => ({arr}))}
+        </div>
+    </>);
 }
 
 export default App;
---
+++ sudoku-front/src/api/sudoku.js
@@ -0,0 +1,7 @@
+import api from "./api"
+
+export function fetchSudoku({params}) {
+    return api.get(`/sudoku/random`, {
+        params
+    });
+}
