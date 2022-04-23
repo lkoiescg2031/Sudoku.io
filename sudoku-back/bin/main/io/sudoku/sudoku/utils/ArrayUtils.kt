135c6859d5083db520a0fa2adbcdc7a312ee1b68
Taehong Kim <lkoiescg2031@naver.com>
2022-04-23 16:41:11 +0900

    refactor : bin 파일 삭제

--- sudoku-back/bin/main/io/sudoku/sudoku/utils/ArrayUtils.kt
+++
@@ -1,4 +0,0 @@
-package io.sudoku.sudoku.utils
-
-inline fun <K, V> List<V>.groupByIndexed(keySelector: (Int, V) -> K): Map<K, List<V>>
-= this.withIndex().groupBy({ (index, value) -> keySelector(index, value) }) { it.value }
